5ff9ec4f7a26c801980c230ca3c8a3bf0a81251c
dakodeon <dakodeon@hotmail.com>
2020-12-07 12:40:49 +0200

    minor color change

--- config.h
+++ config.h
@@ -13,7 +13,7 @@ static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
 	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
+	[SchemeSelHighlight] = { "#ffc978", "#315927" },
 	[SchemeNormHighlight] = { "#ffc978", "#222222" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
